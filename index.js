5988da6099e8095525d6e9d23c36bfcd9bf76904
Amit pandey <amitvedprakashpandey@gmail.com>
2024-07-17 12:53:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const mongoUrl = process.env.DB_URL;
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', true);
 mongoose
   .connect(mongoUrl, {
     useNewUrlParser: true})
